3347e1dfe6e4096763d5ab1d16d22bba9aa489b8
Mustafa Dalga <mustafadalgaa@gmail.com>
2023-05-29 17:45:40 +0300

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
-    <Suspense>
-        <Users/>
-
-        <template #fallback>
-            <div>Loading...</div>
-        </template>
-    </Suspense>
+ <section>
+    <h1 data-testid="h1-1">Hakan</h1>
+    <h1 data-testid="h1-2">Hakan</h1>
+    <h1 data-testid="h1-3">Hakan</h1>
+    <h1 data-testid="h1-4">Hakan</h1>
+    <h1 data-testid="h1-5">Hakan</h1>
+    <h1 data-testid="h1-6">Hakan</h1>
+    <h1 data-testid="h1-7">Hakan</h1>
+    <h1 data-testid="h1-8">Hakan</h1>
+    <h1 data-testid="h1-9">Hakan</h1>
+    <h1 data-testid="h1-11">Hakan</h1>
+ </section>
 </template>
 
 <script setup lang="ts">
