0910bc1dbab097582e85c0beb9e81b007423b02d
Mustafa Dalga <mustafadalgaa@gmail.com>
2024-03-29 11:42:07 +0300

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,256 @@
 <script setup lang="ts">
-import { computed } from "vue";
-import { RouterView, useRoute } from "vue-router";
+import MovieCard from "@/components/MovieCard.vue";
+import { computed, ref } from "vue";
 
-const route = useRoute();
-const skils = [
-  "TypeScript",
-  "JavaScript",
-  "Vue.js ",
-  "React.js ",
-  "Next.js ",
-  "Vuex",
-  "Pinia",
-  "exception",
-  "Redux",
-  "Zustand",
-  "SCSS",
-  "CSS ",
-  "Tailwind CSS",
-  "exception",
-  "Vitest",
-  "Testing Library",
-  "Vue Test Utils"
+const movies = [
+  {
+    "title": "The Shawshank Redemption",
+    "kind": "Adventure"
+  },
+  {
+    "title": "The Godfather",
+    "kind": "War"
+  },
+  {
+    "title": "The Dark Knight",
+    "kind": "Thriller"
+  },
+  {
+    "title": "Schindler's List",
+    "kind": "Adventure"
+  },
+  {
+    "title": "Pulp Fiction",
+    "kind": "Romantic"
+  },
+  {
+    "title": "The Lord of the Rings: The Return of the King",
+    "kind": "Thriller"
+  },
+  {
+    "title": "The Good, the Bad and the Ugly",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "Fight Club",
+    "kind": "War"
+  },
+  {
+    "title": "Forrest Gump",
+    "kind": "Comedy"
+  },
+  {
+    "title": "Inception",
+    "kind": "Animation"
+  },
+  {
+    "title": "Star Wars: Episode V - The Empire Strikes Back",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "The Matrix",
+    "kind": "Animation"
+  },
+  {
+    "title": "Goodfellas",
+    "kind": "Romantic"
+  },
+  {
+    "title": "One Flew Over the Cuckoo's Nest",
+    "kind": "Romantic"
+  },
+  {
+    "title": "Seven Samurai",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "Se7en",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "The Silence of the Lambs",
+    "kind": "War"
+  },
+  {
+    "title": "City of God",
+    "kind": "Thriller"
+  },
+  {
+    "title": "Life is Beautiful",
+    "kind": "Action"
+  },
+  {
+    "title": "It's a Wonderful Life",
+    "kind": "Romantic"
+  },
+  {
+    "title": "The Usual Suspects",
+    "kind": "Animation"
+  },
+  {
+    "title": "Léon: The Professional",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "Saving Private Ryan",
+    "kind": "Action"
+  },
+  {
+    "title": "The Green Mile",
+    "kind": "War"
+  },
+  {
+    "title": "Interstellar",
+    "kind": "Thriller"
+  },
+  {
+    "title": "The Prestige",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "The Departed",
+    "kind": "Action"
+  },
+  {
+    "title": "The Pianist",
+    "kind": "Thriller"
+  },
+  {
+    "title": "Gladiator",
+    "kind": "Romantic"
+  },
+  {
+    "title": "American History X",
+    "kind": "Action"
+  },
+  {
+    "title": "The Lion King",
+    "kind": "Thriller"
+  },
+  {
+    "title": "The Intouchables",
+    "kind": "Drama"
+  },
+  {
+    "title": "Whiplash",
+    "kind": "Sci-Fi"
+  },
+  {
+    "title": "The Godfather: Part II",
+    "kind": "Animation"
+  },
+  {
+    "title": "The Sixth Sense",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "The Social Network",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "The Lives of Others",
+    "kind": "Thriller"
+  },
+  {
+    "title": "A Beautiful Mind",
+    "kind": "War"
+  },
+  {
+    "title": "The Terminator",
+    "kind": "Adventure"
+  },
+  {
+    "title": "The Truman Show",
+    "kind": "Comedy"
+  },
+  {
+    "title": "Blade Runner",
+    "kind": "Comedy"
+  },
+  {
+    "title": "Braveheart",
+    "kind": "Sci-Fi"
+  },
+  {
+    "title": "Jurassic Park",
+    "kind": "Comedy"
+  },
+  {
+    "title": "Toy Story",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "Finding Nemo",
+    "kind": "Thriller"
+  },
+  {
+    "title": "The Wizard of Oz",
+    "kind": "Comedy"
+  },
+  {
+    "title": "Casablanca",
+    "kind": "Thriller"
+  },
+  {
+    "title": "Gone with the Wind",
+    "kind": "Fantasy"
+  },
+  {
+    "title": "The Sound of Music",
+    "kind": "Animation"
+  },
+  {
+    "title": "Raiders of the Lost Ark",
+    "kind": "Fantasy"
+  }
 ]
+const kinds = [ "Action", "Romantic", "War", "Comedy", "Drama", "Thriller", "Adventure", "Sci-Fi", "Fantasy", "Animation" ];
+const form = ref({
+  kind: "",
+  title: ""
+})
+const filteredMovies = computed(() => {
+  return movies.filter((movie) => (!form.value.kind || (form.value.kind && movie.kind == form.value.kind)) && movie.title.toLowerCase().includes(form.value.title.toLowerCase()))
+})
 </script>
 
 <template>
-  <div class="flex flex-wrap p-20 gap-8 text-3xl">
+  <main class="p-10 grid gap-5">
+    <ul class="flex items-center gap-3 flex-wrap">
+      <li @click="form.kind=''"
+          class="text-center cursor-pointer w-32 px-5 py-2 bg-indigo-200 hover:bg-indigo-300 rounded text-sm font-semibold text-gray-700">
+        Clear Filter
+      </li>
+      <li v-for="kind in kinds" :key="kind"
+          @click="form.kind=kind"
+          class="text-center cursor-pointer w-32 px-5 py-2 bg-indigo-200 hover:bg-indigo-300 rounded text-sm font-semibold text-gray-700">
+        {{ kind }}
+      </li>
+    </ul>
+    <div>
+      <input type="text" v-model="form.title">
+    </div>
+    <div class=" grid grid-cols-3 gap-5">
+      <TransitionGroup name="list">
+        <MovieCard v-for="movie in filteredMovies" image="https://source.unsplash.com/random" :movie="movie"
+                   :key="movie"/>
+      </TransitionGroup>
+    </div>
+  </main>
 
-    <template v-for="skill in skils">
-      <div v-if="skill!='exception'" :key="skill"
-           class="backdrop-filter backdrop-blur-md bg-white bg-opacity-10 dark:bg-gray-800 px-5 py-1.5 rounded-lg text-white border border-white border-opacity-75">
-        {{ skill }}
-      </div>
-
-      <div v-else class="w-52"></div>
-    </template>
-  </div>
 </template>
+
+<style>
+.list-move,
+.list-enter-active,
+.list-leave-active {
+  transition: 0.3s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateY(10px);
+}
+
+</style>
---
+++ src/components/MovieCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({
+  movie: String
+})
+
+const keywords=[ "nature", "city", "technology", "food", "animals", "architecture", "travel", "business", "music" ] // Additional keywords
+const randomIndex = Math.floor(Math.random() * keywords.length);
+const id = keywords[randomIndex];
+</script>
+
+<template>
+  <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <img :src="`https://source.unsplash.com/400x300/?${id}`" alt="Random Image">
+
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">{{ movie.title }}</div>
+      <p class="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
+        eaque, exercitationem praesentium nihil.
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
